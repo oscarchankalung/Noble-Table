4826abb40f23fbb4d3efc5cf6b856471f9994c37
oscarchankalung <oscarchankalung@gmail.com>
2024-11-21 16:57:27 -0500

    end at 1700

--- 20241119-python-web-development-with-django/django_rests/businesses/businesses/urls.py
+++ 20241119-python-web-development-with-django/django_rests/businesses/businesses/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from dining import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.RestaurantListView.as_view(), name="restaurant_list"),
+    path("restaurant/<int:pk>/", views.RestaurantDetailView.as_view(), name="restaurant_detail"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 20241119-python-web-development-with-django/django_rests/businesses/dining/views.py
+++ 20241119-python-web-development-with-django/django_rests/businesses/dining/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Restaurant
+
+class RestaurantListView(ListView):
+    model = Restaurant
+
+class RestaurantDetailView(DetailView):
+    model = Restaurant
